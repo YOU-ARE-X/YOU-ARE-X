2c605a8ee56c4f048b1dd45116d92d09075a3576
Simon Demeule <simondemeule@gmail.com>
2019-10-06 18:20:29 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/YOU-ARE-X/YOU-ARE-X""
    This reverts commit c838ce1dba5173053e4c8fd6eea560a593ebccd1.

---
+++ YOU ARE X PROJECT/Assets/Scripts/Element.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	/*
+	Holds information about element:
+	name,
+	audio set(?),
+	zoom level,
+	chronologic sequencing.
+	*/
+
+public class Element {
+
+	public string elementName;
+	public List<ElementScene> scenes = new List<ElementScene>();
+
+	public void init(string n) {
+		elementName = n;
+	}
+
+	public void createScene(string s, string z, int sq) {
+		ElementScene scene = new ElementScene();
+		scene.init(s, z, sq);
+		scenes.Add(scene);
+	}
+}
+
+public class ElementScene {
+
+	public string sceneName;
+	public string zoom;
+	public int sequence;
+
+	public void init(string s, string z, int sq) {
+		sceneName = s;
+		zoom = "x" + z;
+		sequence = sq;
+	}
+}
