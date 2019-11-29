c991b120572c9ca8a7430f1e50f4a480496cf84f
Simon Demeule <simondemeule@gmail.com>
2019-11-28 23:32:12 -0500

    Added eye discarder

---
+++ YOU ARE X/Assets/Scripts/EyeDiscarder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Cam.Effects {
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(UnityEngine.Camera))]
+	[AddComponentMenu("Image Effects/Custom/EyeDiscarder")]
+
+	public class EyeDiscarder : MonoBehaviour {
+
+        private bool currentEye = true;
+        private RenderTexture copiedEye;
+
+        public void OnRenderImage(RenderTexture src, RenderTexture dest) {
+            if(currentEye) {
+				Graphics.Blit(src, dest);
+                Graphics.Blit(src, copiedEye);
+			} else {
+				Graphics.Blit(copiedEye, dest);
+			}
+			currentEye = !currentEye;
+		}
+	}
+}
